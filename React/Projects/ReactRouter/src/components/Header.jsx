203c88c422bbb853d26c1823e0c186deda15f97f
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2026-04-15 16:08:15 +0530

    Update Header.jsx

--- React/Projects/ReactRouter/src/components/Header.jsx
+++ React/Projects/ReactRouter/src/components/Header.jsx
@@ -1,13 +1,25 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'     //Just alternative of Link/NavLink
+
 // Link → used for navigation without page reload (SPA advantage)
 // NavLink → same as Link + highlights active/current page
 
+// useNavigate → navigate using function (used in buttons, logic)
+// useLocation → gives current URL (used to manually check active route)
 
 export default function Header() {
+
+    const navigate = useNavigate()
+    const location = useLocation()   // gives current path 
+
+
     return (
         <header className="shadow sticky z-50 top-0">
+
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+
                     <Link to="/" className="flex items-center">
                         <img
                             src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
@@ -15,20 +27,21 @@ export default function Header() {
                             alt="Logo"
                         />
                     </Link>
+
                     <div className="flex items-center lg:order-2">
-                        <Link
-                            to="#"
+                        <Link to="#"
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
                         </Link>
-                        <Link
-                            to="#"
+
+                        <Link to="#"
                             className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Get started
                         </Link>
                     </div>
+
                     <div
                         className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
@@ -36,7 +49,7 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                to={'/'}
+                                    to={'/'}
                                     className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
@@ -44,29 +57,32 @@ export default function Header() {
                                     Home
                                 </NavLink>
                             </li>
+
                             <li>
-                                <NavLink
-                                to={'/about'}
-                                    className={({ isActive }) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
+                                {/* useNavigate → navigation using function */}
+                                {/* useLocation → manual active check (replacement for isActive) */}
+                                <button
+                                    onClick={() => navigate('/about')}
+                                    className={`block py-2 pr-4 pl-3 duration-200 ${location.pathname === '/about' ? "text-orange-700" : "text-gray-700"} 
+                                        border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`}
                                 >
                                     About
-                                </NavLink>
+                                </button>
                             </li>
+
                             <li>
                                 <NavLink
-                                to={'/contact'}
-                                    className={({ isActive }) =>
+                                    to={'/contact'} className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Contact
                                 </NavLink>
                             </li>
+
                             <li>
                                 <NavLink
-                                to={'/github'}
+                                    to={'/github'}
                                     className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
@@ -74,8 +90,6 @@ export default function Header() {
                                     Github
                                 </NavLink>
                             </li>
-
-
                         </ul>
                     </div>
                 </div>
